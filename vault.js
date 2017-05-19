3a36c88d8e412840bab8e2078d7990bc16dc84df
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-05-18 19:37:11 -1000

    pass

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+var lock = {};
+  function setValue(key, value){
+    lock[key] = value;
+    return key;
+  }
+  function getValue(key){
+  if(lock.hasOwnProperty(key)){
+  return lock[key];
+}else{
+  return null;
+}
+}
+return {
+  setValue: setValue,
+  getValue: getValue
 };
+}
